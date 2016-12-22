2c5ee00ab38afe35232237aa05c61dcf9d2e8220
Tristan King <tristan@bakkenbaeck.no>
2016-12-22 13:59:20 +0100

    move code needed by other services to another package

--- tokenid/app.py
+++ tokenid/app.py
@@ -1,8 +1,9 @@
 from . import handlers
 from asyncbb.web import Application
+from tokenservices.handlers import GenerateTimestamp
 
 urls = [
-    (r"^/v1/timestamp/?$", handlers.GenerateTimestamp),
+    (r"^/v1/timestamp/?$", GenerateTimestamp),
     (r"^/v1/user/?$", handlers.UserCreationHandler),
     (r"^/v1/user/(?P<username>[^/]+)/?$", handlers.UserHandler),
 ]
--- tokenid/handlers.py
+++ tokenid/handlers.py
@@ -1,48 +1,16 @@
 import names as namegenerator
 import regex
-import time
 
 from asyncbb.handlers import BaseHandler
 from asyncbb.database import DatabaseMixin
 from asyncbb.errors import JSONHTTPError
-from tokenbrowser.utils import flatten_payload, data_decoder, parse_int
-from tokenbrowser.crypto import ecrecover
+from tokenservices.handlers import RequestVerificationMixin
 from tornado.escape import json_encode
-
-# used to validate the timestamp in requests. if the difference between
-# the timestamp and the current time is greater than this the reuqest
-# is rejected
-TIMESTAMP_EXPIRY = 15
 
 def validate_username(username):
     return regex.match('^[a-zA-Z][a-zA-Z0-9_]{2,59}$', username)
 
-class GenerateTimestamp(BaseHandler):
-
-    def get(self):
-        self.write({"timestamp": int(time.time())})
-
-class UserMixin:
-
-    def verify_payload(self, expected_address, signature, payload):
-
-        try:
-            signature = data_decoder(signature)
-        except Exception:
-            raise JSONHTTPError(400, body={'errors': [{'id': 'invalid_signature', 'message': 'Invalid Signature'}]})
-
-        address = ecrecover(flatten_payload(payload), signature)
-
-        if address is None or address != expected_address:
-            raise JSONHTTPError(400, body={'errors': [{'id': 'invalid_signature', 'message': 'Invalid Signature'}]})
-
-        # check timestamp
-        if 'timestamp' not in payload:
-            raise JSONHTTPError(400, body={'errors': [{'id': 'invalid_timestamp', 'message': 'The difference between the timestamp and the current time is too large'}]})
-
-        timestamp = parse_int(payload['timestamp'])
-        if timestamp is None or abs(int(time.time()) - timestamp) > TIMESTAMP_EXPIRY:
-            raise JSONHTTPError(400, body={'errors': [{'id': 'invalid_signature', 'message': 'Invalid Signature'}]})
+class UserMixin(RequestVerificationMixin):
 
     async def update_user(self, address):
 
